8d5b52759fe46ec60b14956586a147d7d00b9462
ArryPutra <arrykusumaputra04@gmail.com>
2026-01-02 15:29:05 +0800

    make dropdown safe

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -7,17 +7,41 @@ type DropdownProps = {
     onChange: React.ChangeEventHandler<HTMLSelectElement>;
 }
 
-export default function Dropdown({ name, label, options, valueSelected, className, onChange }: DropdownProps) {
+export default function Dropdown({
+    name,
+    label,
+    options,
+    valueSelected,
+    className,
+    onChange
+}: DropdownProps) {
+
+    const safeValue =
+        valueSelected && options.includes(valueSelected)
+            ? valueSelected
+            : '';
+
     return (
         <div className={`flex flex-col gap-1.5 items-start w-full ${className}`}>
-            {label && <label className='font-semibold'>{label}</label>}
-            <select value={valueSelected ?? ''} onChange={onChange} name={name} className='py-2 px-3 outline-none rounded-lg border-2 border-gray-300 focus:border-blue-500 focus:ring-2 ring-blue-200 w-full mr-4'>
-                <option value="" disabled>Pilih {label}</option>
+            {label && <label className="font-semibold">{label}</label>}
 
-                {options.map((option) => (
-                    <option key={option} value={option}>{option}</option>
+            <select
+                name={name}
+                value={safeValue}
+                onChange={onChange}
+                className="py-2 px-3 outline-none rounded-lg border-2 border-gray-300
+                   focus:border-blue-500 focus:ring-2 ring-blue-200 w-full"
+            >
+                <option value="" disabled hidden>
+                    Pilih {label}
+                </option>
+
+                {options.map(option => (
+                    <option key={option} value={option}>
+                        {option}
+                    </option>
                 ))}
             </select>
         </div>
-    )
+    );
 }
